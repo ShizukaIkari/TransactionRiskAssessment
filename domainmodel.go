c60d5ee486ce13e1526371f27a4eb0a6fe780cd4
Serenna Ferrari <serenna.ferrari@hotmail.com>
2024-01-24 14:55:02 -0300

    FIX API logic - using the right reference - and clean up code for better legibility

--- domainmodel.go
+++ domainmodel.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "fmt"
 	"sort"
 
 	mapset "github.com/deckarep/golang-set/v2"
@@ -33,15 +32,15 @@ func (risk RiskLevel) String() string {
 }
 
 type Transaction struct {
-	Transaction_id      uint      `json:"id"`
-	User_id             uint      `json:"user_id"`
-	Dollar_cents_amount int       `json:"amount_us_cents"`
-	Id_card_used        uint      `json:"card_id"`
-	Risk_rate           RiskLevel `json:"transaction_risk"`
+	TransactionId     uint      `json:"id"`
+	UserId            uint      `json:"user_id"`
+	DollarCentsAmount int       `json:"amount_us_cents"`
+	IdCardUsed        uint      `json:"card_id"`
+	RiskRate          RiskLevel `json:"transaction_risk"`
 }
 
 type RiskRate struct {
-	Risk_rates []string `json:"risk_ratings"`
+	RiskRates []string `json:"risk_ratings"`
 }
 
 // implementing custom sorting function for transaction
@@ -53,17 +52,15 @@ func (transactions TransactionsByID) Swap(i, j int) {
 	transactions[i], transactions[j] = transactions[j], transactions[i]
 }
 func (transactions TransactionsByID) Less(i, j int) bool {
-	return transactions[i].Transaction_id < transactions[j].Transaction_id
+	return transactions[i].TransactionId < transactions[j].TransactionId
 }
 
 type TransactionsInput struct {
 	InputTransactions []Transaction `json:"transactions"`
 }
 
-/*
-*  This function maps each unique user id to their corresponding set of transactions
- */
-func relateUserToTransactions(transacts []Transaction) TransactionsPerUserMap {
+// RelateUserToTransactions maps each unique user id to their corresponding set of transactions
+func RelateUserToTransactions(transacts []Transaction) TransactionsPerUserMap {
 	// unique user ids in transaction list
 	userIdSet := mapset.NewSet[uint]()
 	// set with unique transactions, being instantiated for each new user
@@ -74,30 +71,29 @@ func relateUserToTransactions(transacts []Transaction) TransactionsPerUserMap {
 
 	for _, transact := range transacts {
 		// first time seeing this user id
-		if !userIdSet.Contains(transact.User_id) {
+		if !userIdSet.Contains(transact.UserId) {
 			// add to the known ids set
-			userIdSet.Add(transact.User_id)
+			userIdSet.Add(transact.UserId)
 			// new transaction set for new user
 			transactionSet = mapset.NewSet[Transaction]()
 			// the first transaction for this user in their set
 			transactionSet.Add(transact)
 
 			// assign set for UserId key
-			userAndTransct[transact.User_id] = transactionSet
+			userAndTransct[transact.UserId] = transactionSet
 		} else {
-			userSet := userAndTransct[transact.User_id]
+			userSet := userAndTransct[transact.UserId]
 			userSet.Add(transact)
 		}
 	}
 	return userAndTransct
 }
 
-// Checks which risk is greater, returning the ... greater ... one.
+// Checks which risk is greater, returning the greater one.
 func greaterRisk(currentRisk, newRisk RiskLevel) RiskLevel {
 	if currentRisk < newRisk {
 		return newRisk
 	}
-
 	return currentRisk
 }
 
@@ -107,61 +103,74 @@ func riskPerSingleAmount(userTransactions []Transaction) {
 	mediumRiskAmount := 500000
 	highRiskAmount := 1000000
 
-	for _, transac := range userTransactions {
-		if transac.Dollar_cents_amount > highRiskAmount {
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, HIGH)
-		} else if transac.Dollar_cents_amount > mediumRiskAmount {
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, MEDIUM)
-		} else {
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, LOW)
+	for transacIndex := range userTransactions {
+		// shortening the name reference of memory space
+		currentTransaction := &userTransactions[transacIndex]
+		// standard risk if no match for risk rules
+		risk := LOW
+		if currentTransaction.DollarCentsAmount > highRiskAmount {
+			risk = HIGH
+		} else if currentTransaction.DollarCentsAmount > mediumRiskAmount {
+			risk = MEDIUM
 		}
+		currentTransaction.RiskRate = greaterRisk(userTransactions[transacIndex].RiskRate, risk)
 	}
 }
 
-// should I give this function the responsibility to filter the map/transactions for a given user id?
-// for now I'm determining that the transaction list has only  one disctinct user_id
+// Sums up the dollar amount while iterating through user transactions
+// updating their Risk Level according to the risk rules
 func riskPerTotalAmount(userTransactions []Transaction) {
 	// in US cents
 	var totalAmount int
 	mediumRiskAmount := 1000000
 	highRiskAmount := 2000000
 
-	for _, transac := range userTransactions {
-		totalAmount += transac.Dollar_cents_amount
+	// to update values from slice input, do not use the second return of range
+	// it's a copy of the element of the slice, not a reference.
+	for transacIndex := range userTransactions {
+		currentTransaction := &userTransactions[transacIndex]
+		totalAmount += currentTransaction.DollarCentsAmount
 
+		risk := LOW
 		if totalAmount > mediumRiskAmount && totalAmount <= highRiskAmount {
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, MEDIUM)
+			risk = MEDIUM
 		} else if totalAmount > highRiskAmount {
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, HIGH)
-		} else {
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, LOW)
+			risk = HIGH
 		}
-	}
 
+		currentTransaction.RiskRate = greaterRisk(currentTransaction.RiskRate, risk)
+	}
 }
 
-// So ... I think I'll merge this function with the per total amout to avoid using another for loop
-// BUT if I do this, I think I'll be breaking the single responsibility principle
+// Checks how many different cards are in the user transaction set
+// updating each transaction Risk Level according to the risk rules
 func riskPerMultipleCards(userTransactions []Transaction) {
 	cardIdSet := mapset.NewSet[uint]()
 
-	for _, transac := range userTransactions {
-		if !cardIdSet.Contains(transac.Id_card_used) && cardIdSet.Cardinality() < 1 {
-			// add to the known ids set
-			cardIdSet.Add(transac.Id_card_used)
-			// first transaction to be analyzed has only one card used. so risk level is low
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, LOW)
-		} else if !cardIdSet.Contains(transac.Id_card_used) && cardIdSet.Cardinality() == 1 {
-			cardIdSet.Add(transac.Id_card_used)
-			// now user has used more than 1 card
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, MEDIUM)
-		} else if !cardIdSet.Contains(transac.Id_card_used) && cardIdSet.Cardinality() >= 2 {
-			transac.Risk_rate = greaterRisk(transac.Risk_rate, HIGH)
-		}
-	}
+	for transacIndex := range userTransactions {
+		currentTransaction := &userTransactions[transacIndex]
+		isCardInSet := cardIdSet.Contains(currentTransaction.IdCardUsed)
 
+		if !isCardInSet {
+			cardIdSet.Add(currentTransaction.IdCardUsed)
+		}
+
+		risk := LOW
+		if cardIdSet.Cardinality() <= 1 {
+			risk = LOW
+		} else if cardIdSet.Cardinality() == 2 {
+			risk = MEDIUM
+		} else {
+			risk = HIGH
+		}
+
+		currentTransaction.RiskRate = greaterRisk(currentTransaction.RiskRate, risk)
+	}
 }
 
+// Receives a slice with all transactions that were sent to the API
+// with risks calculated and returns a slice with resultant risk in strings
+// sorted by transaction id * might change
 func allTransactionsRisk(allTransactions []Transaction) RiskRate {
 	var calcRiskRates RiskRate
 	// sorting transactions by ID to maintain the order of input *
@@ -170,18 +179,18 @@ func allTransactionsRisk(allTransactions []Transaction) RiskRate {
 	var risksSlice []string
 
 	for _, transac := range allTransactions {
-		risksSlice = append(risksSlice, transac.Risk_rate.String())
+		risksSlice = append(risksSlice, transac.RiskRate.String())
 	}
-	calcRiskRates.Risk_rates = risksSlice
+	calcRiskRates.RiskRates = risksSlice
 
 	return calcRiskRates
 }
 
 // Receives the input transactions, returns a slice with each transaction risk level ordered by transactionId
-func checkTransactions(userTransactions TransactionsPerUserMap) RiskRate {
+func CheckTransactions(userTransactions TransactionsPerUserMap) RiskRate {
 	var transactionsSlice []Transaction
 
-	// key, value - no use for key, so _ instead. Transactions is a set
+	// key, value - no use for key, so _ instead. Transactions is the copy of the set
 	for _, transactions := range userTransactions {
 		transactSlice := transactions.ToSlice()
 		riskPerSingleAmount(transactSlice)
